6f738ca639676dfa6dc4993f8bdb79d854f20abf
Nabin Gyawali <121758029+Nabin-gyawali@users.noreply.github.com>
2025-02-17 18:35:35 +0545

    Optimized Quotes Card & Enhanced UI/UX
    HTML: Converted the entire quotes card into a button by removing the extra button for generating random quotes.
    CSS: Fixed minor design issues, added a rotating hover effect to the Arc Reactor logo, improved mobile responsiveness with media queries, and refined the styling of the quotes generator.
    JS: Removed excessively long quotes for better readability.

--- Script.js
+++ Script.js
@@ -7,8 +7,6 @@ const quotes = [
     "It's not about how much we lost. It's about how much we have left.",
     "Following’s not really my style.",
     "I love you 3000.",
-    "It’s an imperfect world, but it’s the only one we got. I guarantee you the day weapons are no longer needed to keep the peace, I’ll start making bricks and beams for baby hospitals.",
-    "I saw young Americans killed by the very weapons I created to defend them and protect them. And I saw that I had become part of a system that is comfortable with zero accountability.",
     "Everybody wants a happy ending. Right? But it doesn’t always roll that way.",
     "What’s the point of owning a race car if you can’t drive it?"
 ];
